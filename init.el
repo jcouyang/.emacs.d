b7e5605761b0724ea776565fe4422f19fe0374c2
Jichao Ouyang <oyanglulu@gmail.com>
2015-10-29 21:27:38 +0800

    sass mode ace jump

--- init.el
+++ init.el
@@ -12,8 +12,10 @@
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
  '(electric-pair-mode t)
+ '(js2-basic-offset 2 t)
  '(standard-indent 2)
- '(web-mode-code-indent-offset 2))
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-markup-indent-offset 4))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
